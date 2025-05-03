90e3e57023054a375396dd6fe0535cc0f945ec2f
cBee12345 <c.balsomo.544358@umindanao.edu.ph>
2025-05-03 20:39:11 +0800

    added a class

---
+++ Consultation.Domain/Bulletin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultation.Domain
+{
+    public class Bulletin
+    {
+
+        [Key]
+        public int BulletinID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public DateTime DatePublished { get; set; }
+
+        public int Priority { get; set; }
+
+        public string Notify { get; set; }
+        
+    }
+}
