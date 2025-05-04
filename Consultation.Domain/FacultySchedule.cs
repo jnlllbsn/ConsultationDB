2674c8526820bf9ecfa0b283545fdbe12baddc3e
rianeksr19 <r.isid.545154@umindanao.edu.ph>
2025-05-04 09:02:03 +0800

    FacultySchedule

---
+++ Consultation.Domain/FacultySchedule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultation.Domain
+{
+    public class FacultySchedule
+    {
+        [Key]
+        public int FacultyScheduleId { get; set; }
+        public DateTime TimeStart { get; set; }
+        public DateTime TimeEnd { get; set; }
+
+        public List<DayOfWeek> AvailableDays { get; set; } = new List<DayOfWeek>
+        {
+            DayOfWeek.Monday,
+            DayOfWeek.Tuesday,
+            DayOfWeek.Wednesday,
+            DayOfWeek.Thursday,
+            DayOfWeek.Friday,
+            DayOfWeek.Saturday
+        };
+
+        public DateOnly Day { get; set; }
+
+        [ForeignKey(nameof(Faculty))]
+        public int FacultyID { get; set; }
+        public Faculty Faculty { get; set; }
+
+
+    }
+}
